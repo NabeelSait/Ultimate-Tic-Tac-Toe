eb3f48c6c3cdffc3685cbfeb8a2de32441e5c5d1
Curtis Dawkins <curtis.dawkins@slu.edu>
2019-11-06 12:35:38 -0600

    added HumanPlayer.java

---
+++ Model/HumanPlayer.java
@@ -0,0 +1,24 @@
+public class HumanPlayer extends Player{
+   private String name;
+   
+   Player p = new Player(h, 1);
+   
+   HumanPlayer(char t, int n, String nm)
+   {
+      super(t, n);
+      name = nm;
+   }
+
+   public void setName(String nm)
+   {
+      name = nm;
+   {
+
+   public String getName()
+   {
+      return name;
+   }
+
+
+
+}
