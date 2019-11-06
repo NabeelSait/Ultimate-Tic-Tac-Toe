b9d7bfae4514d236b48fce7dcdc04ece6adb29bd
Curtis Dawkins <curtis.dawkins@slu.edu>
2019-11-06 12:40:37 -0600

    updated Human Player

--- Model/HumanPlayer.java
+++ Model/HumanPlayer.java
@@ -1,24 +1,22 @@
 public class HumanPlayer extends Player{
+   
    private String name;
    
-   Player p = new Player(h, 1);
    
-   HumanPlayer(char t, int n, String nm)
-   {
+   
+   public HumanPlayer(char t, int n, String nm){
       super(t, n);
       name = nm;
    }
 
-   public void setName(String nm)
-   {
+   public void setName(String nm){
       name = nm;
    {
 
-   public String getName()
-   {
+   public String getName(){
       return name;
    }
 
 
 
-}
+}//end class
