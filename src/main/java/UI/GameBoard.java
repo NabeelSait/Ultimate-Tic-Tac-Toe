1f9d8a03811cd494bcea8292ef1c8751738e217c
Curtis Dawkins <curtis.dawkins@slu.edu>
2019-11-10 22:25:26 -0600

    Failed attempt at a forfeit button :(

--- src/main/java/UI/GameBoard.java
+++ src/main/java/UI/GameBoard.java
@@ -12,7 +12,18 @@ class GameBoard extends JPanel {
         for (int i = 0; i < 9; i++) {
             _boards[i] = new GamePanel();
             add(_boards[i]);
+	 //Forfeit Button
+        JButton ff = new JButton("Forfeit");
+        this.add(ff);
+        ActionListener a = new ActionListener();
+        ff.addActionListener(a);
+	
         }
+        //Forfeit button stuff. Delete it if you dont need it
+	public void actionPerformed(ActionEvent e)
+	{
+	   //asks user about saving replay and exits the game.	
+	}
 
         setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.black));
         setPreferredSize(new Dimension(600, 600));
