a4936dced3d7d19ba0a09996e43d752d02f75fb9
Nabeel Sait <nabeel.sait@slu.edu>
2019-11-13 22:03:36 -0600

    Sloppy WinCon code added to GamePanelModel

--- src/main/java/Model/GamePanelModel.java
+++ src/main/java/Model/GamePanelModel.java
@@ -17,14 +17,60 @@ public class GamePanelModel implements GameModel {
 
     public int checkWinCon(Move m, Player player)
     {
-      if (1 == 1)  //Insert actual wincon checking here
+      int pos = m.getPosition();
+      //Horizontal Right
+      if (((pos + 1) <= 8) & ((pos + 1) >= 0) & ((squares[pos + 1].equals(player.getType()))))
       {
-         return 1;
+         if (((pos + 2) <= 8) & ((pos + 2) >= 0)) & ((squares[pos +2].equals(player.getType()))))
+         {
+            return 1;
+         }
       }
-      else
+      //Horizontal Left
+      if (((pos - 1) <= 8) & ((pos - 1) >= 0) & ((squares[pos - 1].equals(player.getType()))))
       {
-         return 0;
+         if (((pos - 2) <= 8) & ((pos - 2) >= 0)) & ((squares[pos - 2].equals(player.getType()))))
+         {
+            return 1;
+         }
       }
+      //Vertical Up
+      if (((pos - 3) <= 8) & ((pos - 3) >= 0) & ((squares[pos - 3].equals(player.getType()))))
+      {
+         if (((pos - 3) <= 8) & ((pos - 3) >= 0)) & ((squares[pos - 3].equals(player.getType()))))
+         {
+            return 1;
+         }
+      }
+      //Vertical Down
+      if (((pos + 3) <= 8) & ((pos + 3) >= 0) & ((squares[pos + 3].equals(player.getType()))))
+      {
+         if (((pos + 6) <= 8) & ((pos + 6) >= 0)) & ((squares[pos + 6].equals(player.getType()))))
+         {
+            return 1;
+         }
+      }
+      //Diagonals
+      if (squares[4].equals(player.getType())
+      {
+         if ((pos == 0) & (squares[8].equals(player.getType()))) //Bottom Left to Top Right
+         {
+            return 1;
+         }
+         if ((pos == 8) & (squares[0].equals(player.getType())))
+         {
+            return 1;
+         }
+         if ((pos == 2) & (squares[6].equals(player.getType()))) //Bottom Right to Top Left
+         {
+            return 1;
+         }
+         if ((pos == 6) & (squares[2].equals(player.getType())))
+         {
+            return 1;
+         }
+      }
+      return 0;
     }
 
 }
